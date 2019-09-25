eb4e88d0c160780421463a5814469ea5e33fa85f
niemczyk13 <arkadiusz.niemiec1@gmail.com>
2019-09-25 08:25:20 +0200

    Dodanie TrainStationPlatforms

--- src/main/java/onlineTimetables/entity/stops/train/TrainStation.java
+++ src/main/java/onlineTimetables/entity/stops/train/TrainStation.java
@@ -1,9 +1,7 @@
 package onlineTimetables.entity.stops.train;
 
-import java.util.List;
-
 import onlineTimetables.entity.stops.StopImpl;
 
 public class TrainStation extends StopImpl {
-	private List<String> platformsList;
+	private TrainStationPlatforms platforms;
 }
---
+++ src/main/java/onlineTimetables/entity/stops/train/TrainStationPlatforms.java
@@ -0,0 +1,7 @@
+package onlineTimetables.entity.stops.train;
+
+import java.util.List;
+
+public class TrainStationPlatforms {
+	private List<String> platformsList;
+}
